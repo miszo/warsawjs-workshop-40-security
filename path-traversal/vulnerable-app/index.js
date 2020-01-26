a9f7b584aa796993692010da62b2857e58729532
Miszo Radomski <miszo.radomski@gmail.com>
2020-01-26 14:47:13 +0100

    New: Path traversal on server comment

--- path-traversal/vulnerable-app/index.js
+++ path-traversal/vulnerable-app/index.js
@@ -3,5 +3,20 @@ const express = require('express');
 const app = express();
 
 app.use((express.static('.')));
+// app.get('/download', (request, response) => {
+//     const files = [
+//         'home.html',
+//         'contact.html'
+//     ];
+//
+//     const { page } = request.params;
+//
+//     if (!files.includes(page)) {
+//         console.warn('Access denied!');
+//         return;
+//     }
+//
+//     response.end();
+// });
 
 app.listen('8000');
